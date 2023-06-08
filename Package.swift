ea3ab930dcac315c124cddde7f6c353fd3bb7b9d
Tarik AIT M'BAREK <taitmbarek@gmail.com>
2023-06-08 13:30:13 +0100

    update package.swift

--- Package.swift
+++ Package.swift
@@ -14,6 +14,22 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "KnotAPI",
-            path: "KnotAPI.xcframework")
+            url: "https://github.com/millionscard/knot-api-ios/releases/download/0.3.1/KnotAPI.xcframework.zip")
+    ]
+)
+
+let package = Package(
+    name: "Intercom",
+    products: [
+        .library(
+            name: "Intercom",
+            targets: ["Intercom"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "Intercom",
+            url: "https://github.com/intercom/intercom-ios/releases/download/15.0.0/Intercom.xcframework.zip",
+            checksum: "dc8c092b07debb5189f272a2b242ce8996b06308ecedc4b4f92ec70f3851989a"
+        ),
     ]
 )
